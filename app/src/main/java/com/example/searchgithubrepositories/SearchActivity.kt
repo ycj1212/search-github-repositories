4733c9fe69cfbc7c832fc70cd51957a239861029
Yang Cheol Joo <rlaengks0001@naver.com>
2022-01-06 18:06:03 +0900

    refactor: ViewBinding 적용

--- app/src/main/java/com/example/searchgithubrepositories/SearchActivity.kt
+++ app/src/main/java/com/example/searchgithubrepositories/SearchActivity.kt
@@ -2,10 +2,14 @@ package com.example.searchgithubrepositories
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.searchgithubrepositories.databinding.ActivitySearchBinding
 
 class SearchActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySearchBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_search)
+        binding = ActivitySearchBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
