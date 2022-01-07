aab611ee6ffad0c80d18e6986c5fb6a23b039fd2
Yang Cheol Joo <rlaengks0001@naver.com>
2022-01-07 12:00:19 +0900

    feat: SearchView 생성

--- app/src/main/java/com/example/searchgithubrepositories/SearchFragment.kt
+++ app/src/main/java/com/example/searchgithubrepositories/SearchFragment.kt
@@ -4,11 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
 import com.example.searchgithubrepositories.databinding.FragmentSearchBinding
 
 class SearchFragment : Fragment() {
     private lateinit var binding: FragmentSearchBinding
+    private lateinit var searchView: SearchView
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -17,5 +19,23 @@ class SearchFragment : Fragment() {
     ): View {
         binding = FragmentSearchBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        searchView = binding.tbSearchGithubRepositories.menu.findItem(R.id.search).actionView as SearchView
+        searchView.isSubmitButtonEnabled = true
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextChange(newText: String?): Boolean {
+                TODO("입력 중일 때 처리")
+                return true
+            }
+
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                TODO("입력을 완료할 때 처리")
+                return true
+            }
+        })
     }
 }
