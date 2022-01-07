d77c180b4bd3397b8dc6c27f3a3107fe897ce104
Yang Cheol Joo <rlaengks0001@naver.com>
2022-01-07 14:01:37 +0900

    feat: LiveData 생성

--- app/src/main/java/com/example/searchgithubrepositories/GithubRepoSearchViewModel.kt
+++ app/src/main/java/com/example/searchgithubrepositories/GithubRepoSearchViewModel.kt
@@ -1,5 +1,10 @@
 package com.example.searchgithubrepositories
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class GithubRepoSearchViewModel : ViewModel()
+class GithubRepoSearchViewModel : ViewModel() {
+    private val _githubRepoList = MutableLiveData<List<GithubRepo>>()
+    val githubRepoList: LiveData<List<GithubRepo>> = _githubRepoList
+}
