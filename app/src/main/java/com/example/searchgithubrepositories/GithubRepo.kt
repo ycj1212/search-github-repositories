f75892f56e4938ddfefbd8dfc347b1033434d88b
Yang Cheol Joo <rlaengks0001@naver.com>
2022-01-07 14:02:46 +0900

    feat: API 응답으로부터 데이터 모델 생성 함수 추가

--- app/src/main/java/com/example/searchgithubrepositories/GithubRepo.kt
+++ app/src/main/java/com/example/searchgithubrepositories/GithubRepo.kt
@@ -4,4 +4,14 @@ data class GithubRepo(
     val name: String,
     val imageUrl: String,
     val language: String
-)
+) {
+    companion object {
+        fun from(item: Item) = with(item) {
+            GithubRepo(
+                name = fullName,
+                imageUrl = owner.avatarUrl,
+                language = language
+            )
+        }
+    }
+}
