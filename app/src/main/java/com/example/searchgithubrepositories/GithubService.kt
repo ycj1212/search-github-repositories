d1312ce41c1706a92dc8b9109693a1acd3116672
Yang Cheol Joo <rlaengks0001@naver.com>
2022-01-07 12:27:27 +0900

    feat: API 통신을 위한 인터페이스 추가

---
+++ app/src/main/java/com/example/searchgithubrepositories/GithubService.kt
@@ -0,0 +1,47 @@
+package com.example.searchgithubrepositories
+
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface GithubService {
+    @GET("search/repositories")
+    fun searchRepositories(
+        @Query("q") query: String,
+        @Query("sort") sort: String? = null,
+        @Query("order") order: String? = null,
+        @Query("per_page") perPage: Int? = null,
+        @Query("page") page: Int? = null
+    )
+
+    companion object {
+        private const val BASE_URL = "https://api.github.com/"
+
+        fun create(): GithubService {
+            val logger = HttpLoggingInterceptor().apply {
+                level = HttpLoggingInterceptor.Level.BASIC
+            }
+
+            val client = OkHttpClient.Builder()
+                .addInterceptor(logger)
+                .addInterceptor { chain ->
+                    val request = chain.request().newBuilder()
+                        .addHeader("Accept", "application/vnd.github.v3+json")
+                        .build()
+
+                    chain.proceed(request)
+                }
+                .build()
+
+            return Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .client(client)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+                .create(GithubService::class.java)
+        }
+    }
+}
