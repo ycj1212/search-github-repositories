87df8b4d2694c8efcef9a32438b4078d7bc9f35b
Yang Cheol Joo <rlaengks0001@naver.com>
2022-01-10 23:05:39 +0900

    feat: 반환 타입을 Flow로 수정

--- app/src/main/java/com/example/searchgithubrepositories/data/GithubRepository.kt
+++ app/src/main/java/com/example/searchgithubrepositories/data/GithubRepository.kt
@@ -1,11 +1,20 @@
 package com.example.searchgithubrepositories.data
 
+import androidx.paging.*
 import com.example.searchgithubrepositories.api.GithubService
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 class GithubRepository @Inject constructor(private val service: GithubService) {
-    suspend fun searchRepositories(query: String): List<GithubRepo> {
-        val response = service.searchRepositories(query = query)
-        return response.items.map { item -> GithubRepo.from(item) }
-    }
+    fun searchRepositories(query: String): Flow<PagingData<GithubRepo>> =
+        Pager(
+            PagingConfig(pageSize = 10),
+            pagingSourceFactory = { GithubRepoPagingSource(service, query) }
+        ).flow
+            .map { pagingData ->
+                pagingData.map { item ->
+                    GithubRepo.from(item)
+                }
+            }
 }
